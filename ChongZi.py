829252a0a9571139f7672128150a2e356e19f5ef
William Miller <Wmilleiv@gmail.com>
2024-10-26 19:44:10 -0400

    selenium base

--- ChongZi.py
+++ ChongZi.py
@@ -1,15 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
-r=requests.get('http://www.baobao88.com/list/21/4--0--155--.html')
+#r=requests.get('http://www.baobao88.com/list/21/4--0--155--.html')
 
-print(r)
-soup=BeautifulSoup(r.content,'html.parser')
-print(soup.prettify())
+#soup=BeautifulSoup(r.content,'html.parser')
+driver=webdriver.Firefox()
+driver.get('http://www.baobao88.com/list/21/4--0--155--.hml')
 
-s=soup.find('div', class_='list_right_nr1')
-for link in s.find_all('a'):
-    print(link.get('href'))
+links = driver.find_elements(By.CLASSNAME, "tip")
+
+for link in link:
+    link.click()
+    stories.append(driver.find_element_by_id("tuimg"))
+
+#for story in stories
+print(stories)
+
+#s=soup.find('div', class_='list_right_nr1')
+#for link in s.find_all('a'):
+    #print(link.get('href'))
 
 
 
